c72f94460bdb366f8f49c43c3ea9ea9a24cb6832
lenow55 <99322377+lenow55@users.noreply.github.com>
2026-08-17 07:38:29 +0300

    fix: make required empty list explicitly

--- lua/codecompanion/_extensions/automcp/tools.lua
+++ lua/codecompanion/_extensions/automcp/tools.lua
@@ -114,7 +114,7 @@ function M.list_tool_groups()
 				parameters = {
 					type = "object",
 					properties = vim.empty_dict(),
-					required = vim.empty_dict(),
+					required = vim.empty_list(),
 					additionalProperties = false,
 				},
 				strict = true,
